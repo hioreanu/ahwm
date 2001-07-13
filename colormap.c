dd58f65be3c446806d606bfe494f1d1995feaa69
Alex Hioreanu <git@hioreanu.net>
2001-07-13 08:14:56 +0000

    added them, don't think I'll implement anything any time soon

---
+++ colormap.c
@@ -0,0 +1,15 @@
+/* $Id$
+ * This was written by Alex Hioreanu in 2001.
+ * This code is in the public domain and the author disclaims all
+ * copyright privileges.
+ */
+
+#include <stdio.h>
+
+#include "colormap.h"
+#include "client.h"
+
+void colormap_install(client_t *client)
+{
+    fprintf(stderr, "XWM: FIXME\n");
+}
---
+++ colormap.h
@@ -0,0 +1,19 @@
+/* $Id$
+ * This was written by Alex Hioreanu in 2001.
+ * This code is in the public domain and the author disclaims all
+ * copyright privileges.
+ */
+
+/*
+ * WARNING:  This code is VERY untested, as all of my machines run at
+ * 24 bit depth
+ */
+
+#ifndef COLORMAP_H
+#define COLORMAP_H
+
+#include "client.h"
+
+void colormap_install(client_t *client);
+
+#endif /* COLORMAP_H */
