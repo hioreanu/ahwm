5354fc249a1e90f1f9cbe66d243a499c308c8806
Alex Hioreanu <git@hioreanu.net>
2001-12-28 20:57:29 +0000

    can also get BadDrawable on XDrawLine

--- xwm.c
+++ xwm.c
@@ -290,6 +290,8 @@ static int error_handler(Display *dpy, XErrorEvent *error)
         || (error->request_code == X_PolyText8
             && error->error_code == BadDrawable)
         || (error->request_code == X_PolyFillRectangle
+            && error->error_code == BadDrawable)
+        || (error->request_code == X_PolySegment
             && error->error_code == BadDrawable))
         return 0;
     fprintf(stderr, "XWM: ");
